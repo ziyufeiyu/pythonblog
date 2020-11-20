d35746c84cda6b5a0b9b2a1d48888b524ee129a7
caojunfeng <caoziang0301@163.com>
2020-11-20 14:48:18 +0800

    增加文件的默认文件

--- blog/models.py
+++ blog/models.py
@@ -52,7 +52,8 @@ class Article(models.Model):
     # 使用外键关联分类表与分类是一对多关系
     tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True, null=True)
     # 使用外键关联标签表与标签是多对多关系
-    img = models.ImageField(upload_to='article_img/%Y/%m/%d/', verbose_name='文章图片', blank=True, null=True)
+    img = models.ImageField(upload_to='article_img/%Y/%m/%d/', default='article_img/2020/11/20/c_language_tYPCGxB.jpg',
+                            verbose_name='文章图片', blank=True, null=True)
     # body = models.TextField()
     body = UEditorField('内容', width=800, height=500,
                         toolbars="normal", imagePath="", filePath="",

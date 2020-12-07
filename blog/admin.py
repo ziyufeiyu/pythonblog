bf6f97f4bafc2dc7618cb1e9c05231cfef5e8336
caojunfeng <caoziang0301@163.com>
2020-12-07 15:21:32 +0800

    修改文章展现为更新时间

--- blog/admin.py
+++ blog/admin.py
@@ -7,12 +7,12 @@ from .models import Banner, Category, Tag, Recommend, Article, Link
 # 导入需要管理的数据库表
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'title', 'recommend', 'user', 'views', 'created_time')
+    list_display = ('id', 'category', 'title', 'recommend', 'user', 'views', 'modified_time')
     exclude = ('summary',)
     # 文章列表里显示想要显示的字段
     list_per_page = 50
     # 满50条数据就自动分页
-    ordering = ('-created_time',)
+    ordering = ('-modified_time',)
     # 后台数据列表排序方式
     list_display_links = ('id', 'title')
     # 设置哪些字段可以点击进入编辑界面
